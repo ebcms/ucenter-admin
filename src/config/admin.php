0459d82036c47a4e70aabaeabda968cac906243e
John Doe <johndoe@example.com>
2022-03-22 14:31:15 +0800

    1

--- src/config/admin.php
+++ src/config/admin.php
@@ -18,7 +18,6 @@ return [
         $res[] = [
             'title' => '用户消息',
             'url' => $router->build('/ebcms/ucenter-admin/message/index'),
-            'tags' => ['menu'],
             'priority' => 30,
             'icon' => 'data:image/svg+xml;base64,' . base64_encode('<svg width="1024" height="1024" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:se="http://svg-edit.googlecode.com" class="icon"><g class="layer"><title>Layer 1</title><g id="svg_3"><path class="selected" d="m76.21947,230.9546c1.34507,1.5599 2.75222,3.05097 4.24213,4.51911l338.74956,333.77227a139.67987,154.8428 0 0 0 185.57766,0l338.74956,-333.77227c1.48992,-1.46814 2.89706,-2.95922 4.22143,-4.49618c0.49664,3.99151 0.74496,8.07477 0.74496,12.22685l0,537.56833a82.77326,91.75869 0 0 1 -82.77326,91.75869l-707.46305,0a82.77326,91.75869 0 0 1 -82.77326,-91.75869l0,-537.56833c0,-3.07391 0.14485,-6.10196 0.41387,-9.10704l0.31039,-3.14274z" data-spm-anchor-id="a313x.7781069.0.i0" fill="#279CFF" id="svg_1"/><path d="m566.98213,476.75321l330.16184,-325.28459l-770.28795,0l330.16184,325.28459a82.77326,91.75869 0 0 0 109.96427,0z" data-spm-anchor-id="a313x.7781069.0.i1" fill="#279CFF" fill-opacity="0.5" id="svg_2"/></g></g></svg>'),
         ];
